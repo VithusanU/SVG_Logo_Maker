2b0c30afb60db5e8d166b448651077396e6af473
VithusanU <130177580+VithusanU@users.noreply.github.com>
2023-09-30 10:33:53 -0400

    More questions, Title, description, toc, instalation

--- ReadMe_Generator/index.js
+++ ReadMe_Generator/index.js
@@ -2,3 +2,30 @@ const inquirer = reqwuire('inquirer');
 const fs = require('fs');
 const markdown = require('./generateMarkdown');
 
+const questions = [{
+    
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+}, 
+{
+    type: 'input',
+    name: 'description',
+    message: 'Provide a brief description of your project explaining the what, why and how.',
+},
+{
+    type: 'checkbox',
+    name: 'TableOfContents',
+    message: 'What sections do you want to include in the Table of Contents?',
+    choices: ['Installation', 'Usage', 'License', 'Contributing', 'Test', 'Questions'],
+},
+{
+    type:'input',
+    name: 'installation',
+    message: 'What are the steps required to install your project?'
+}
+
+
+
+
+]
